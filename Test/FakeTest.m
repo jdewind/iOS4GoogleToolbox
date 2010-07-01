3a00ff95acc0e6f956f23584de06d791940acbe7
Justin DeWind <dewind@atomicobject.com>
2010-07-01 15:36:49 -0400

    updated project to incorporate new changes from google toolbox and adding new failure oddities

--- Test/FakeTest.m
+++ Test/FakeTest.m
@@ -8,9 +8,19 @@
 @implementation FakeTest
 
 - (void) setUp {
+  mock = [OCMockObject mockForClass:[NSString class]];
 }
 
 - (void) tearDown {
+}
+
+- (void) test_handles_mock_failure {
+  [[mock expect] stringByAppendingString:@"hello"];
+  [mock verify];
+}
+
+- (void) test_handles_hamcrest_assertions {
+  assertThat(@"hello", equalTo(@"world"));
 }
 
 - (void) test_something {
