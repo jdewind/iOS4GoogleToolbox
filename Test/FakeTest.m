c34358d1b04ba9cdc9a3e872a369a4d498457c58
Justin DeWind <dewind@atomicobject.com>
2010-07-02 11:06:51 -0400

    adding various test cases

--- Test/FakeTest.m
+++ Test/FakeTest.m
@@ -22,6 +22,25 @@
   [mock verify];
 }
 
+-(void)test_proxy_and_strategy_object_invocation {
+  Strategy *strategy = [[[Strategy alloc] init] autorelease];
+  id proxyClass = [[[ProxyClass alloc] init] autorelease];
+  
+  @try {
+    [strategy throwAProgramaticException];
+  }
+  @catch (NSException * e) {
+    NSLog(@"Caught strategy object exception");
+  }
+  
+  @try {
+    [proxyClass throwAProgramaticException];
+  }
+  @catch (NSException * e) {
+    NSLog(@"This should not be caught");
+  }
+}
+
 -(void)test_catches_compiler_directive_throw {
   [fakeObject throwAProgramaticException];
 }
@@ -38,4 +57,31 @@
   STFail(@"test something");
 }
 
+- (void) test_catch_and_handle_strategy_object_exceptions {
+  Strategy *strategy = [[[Strategy alloc] init] autorelease];
+ 	NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[[Strategy class] instanceMethodSignatureForSelector:@selector(throwAProgramaticException)]];
+  [invocation setTarget:strategy];
+  [invocation setSelector:@selector(throwAProgramaticException)];
+  
+  @try {
+    objc_msgSend(strategy, [invocation selector]);
+    [invocation invoke];
+  }
+  @catch (NSException * e) {
+    NSLog(@"Caught programatic exception");
+  }
+  
+  invocation = [NSInvocation invocationWithMethodSignature:[[Strategy class] instanceMethodSignatureForSelector:@selector(throwACompilerDirectiveException)]];
+  [invocation setTarget:strategy];
+  [invocation setSelector:@selector(throwAProgramaticException)];
+  
+  @try {
+    [invocation invoke];
+  }
+  @catch (NSException * e) {
+    NSLog(@"Caught compiler exception");
+  }
+  
+  
+}
 @end
